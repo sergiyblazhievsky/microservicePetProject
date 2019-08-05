4ecd847a01bff88c431f51d675b7fbd81ef2cf33
Serhii Blazhyievskyi <cnews.sb@gmail.com>
2019-08-05 15:50:16 +0300

    Initial commit for exchange microservice

---
+++ exchangeRatesService/src/main/java/org/sbgroup/petproject/exchangeRatesService/ExchangeRatesServiceApplication.java
@@ -0,0 +1,13 @@
+package org.sbgroup.petproject.exchangeRatesService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRatesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRatesServiceApplication.class, args);
+	}
+
+}
