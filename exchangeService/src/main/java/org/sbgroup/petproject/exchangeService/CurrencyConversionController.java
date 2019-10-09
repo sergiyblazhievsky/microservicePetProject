fd85b5608abcf8ad4ab4ab253761fe4a7d91707c
Serhii Blazhyievskyi <cnews.sb@gmail.com>
2019-10-09 11:29:47 +0300

    Fixed feign, added coordination with Eureka

--- exchangeService/src/main/java/org/sbgroup/petproject/exchangeService/CurrencyConversionController.java
+++ exchangeService/src/main/java/org/sbgroup/petproject/exchangeService/CurrencyConversionController.java
@@ -3,7 +3,6 @@ package org.sbgroup.petproject.exchangeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,15 +13,6 @@ import java.math.BigDecimal;
 public class CurrencyConversionController
 {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    @Autowired
-    private CurrencyExchangeServiceProxy ratesProxy;
-
-    @Autowired
-    private CurrencyDepositServiceProxy depositProxy;
-
-    @Autowired
-    private Environment environment;
 
     @Autowired
     private CurrencyConversionRepository repository;
